79ccfad8a8d16dccb557c09aaa1556749944d7d9
ramennoodlesxv <124167922+Ramune-Subnautica@users.noreply.github.com>
2023-02-02 21:38:28 +1300

    Major changes to everything

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 document.querySelectorAll('#game-folder').forEach(function(el) {
   el.addEventListener('click', function() {
     Swal.fire({
+      width: 650,
       color: '#ffffff',
-      confirmButtonColor: '#8f8989',
       background: '#282828',
-      confirmButtonText: 'Thanks, found it!',
-      width: 900,
       showCloseButton: true,
       showCancelButton: false,
       showConfirmButton: false,
@@ -20,8 +18,9 @@ document.querySelectorAll('#game-folder').forEach(function(el) {
             color: #ffa801;
           }
         </style>
+        
         <div class="sweet-alert-text-container">
-        <br></br>
+        <br>
           <p><b>Game folder for Steam</b><br></br>Open Steam > Go to your Library > Right click Subnautica > Manage > Browse Local Files</p>
           <img src="https://media.discordapp.net/attachments/1012967749045190696/1070312447409401917/image.png" width="400" height="200">
           <br></br>
